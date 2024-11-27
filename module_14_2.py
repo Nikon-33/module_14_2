6bf9343dbb41854dbbdd0b03a6cbabaac3ac82ca
Nikon-33 <89101717800@mail.ru>
2024-11-27 22:06:24 +0300

    Add files via upload

--- module_14_2.py
+++ module_14_2.py
@@ -32,22 +32,22 @@ users = cursor.fetchall()
 
 for user in users:
     username, email, age, balance = user
-    print(f'Имя: {username} | Почта: {email}, | Возраст: {age}, | Баланс: {balance}')
+    # print(f'Имя: {username} | Почта: {email}, | Возраст: {age}, | Баланс: {balance}')
 
 cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
 cursor.execute("SELECT SUM(balance) FROM Users")
 all_balances = cursor.fetchone()[0]
-print(f'Сумма всех балансов: {all_balances}')
-cursor.execute("SELECT AVG(balance) FROM Users")
-avg_balance = cursor.fetchone()[0]
-print(f'Средний баланс всех пользователей: {avg_balance}')
+# print(f'Сумма всех балансов: {all_balances}')
+# cursor.execute("SELECT AVG(balance) FROM Users")
+# avg_balance = cursor.fetchone()[0]
+# print(f'Средний баланс всех пользователей: {avg_balance}')
 
 cursor.execute("SELECT COUNT(*) FROM Users")
 total_users = cursor.fetchone()[0]
 
 if total_users > 0:
     balance_per_user = all_balances / total_users
-    print(f'Баланс / пользователи = {balance_per_user}')
+    print(balance_per_user)
 else:
     print('Нет пользователей для расчета.')
 
